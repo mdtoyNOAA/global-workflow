982a1f02e3b880be43f4ae8a3f0ffbb34c056aea
Judy.K.Henderson <Judy.K.Henderson@noaa.gov>
2024-01-09 02:26:22 +0000

    Add experiment setup script for p8_ugwpv1

---
+++ workflow/p8_ugwpv1.sh
@@ -0,0 +1,17 @@
+USER=Judy.K.Henderson
+GITDIR=/scratch1/BMC/gsd-fv3-dev/jhender/test/gsl_ufs_dev/            ## where your git checkout is located
+COMROT=$GITDIR/FV3GFSrun                                         ## default COMROT directory
+EXPDIR=$GITDIR/FV3GFSwfm                                         ## default EXPDIR directory
+ICSDIR=/scratch1/BMC/gsd-fv3/rtruns/FV3ICS_L127
+
+PSLOT=p8
+IDATE=2023112800
+EDATE=2023112800
+RESDET=768               ## 96 192 384 768
+
+### gfs_cyc 1  00Z only;  gfs_cyc 2  00Z and 12Z
+
+./setup_expt.py gfs forecast-only --pslot $PSLOT  --gfs_cyc 1 \
+       --idate $IDATE --edate $EDATE --resdet $RESDET \
+       --comrot $COMROT --expdir $EXPDIR
+
